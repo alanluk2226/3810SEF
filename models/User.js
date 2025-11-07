2fdea31c3179942d5803712409066c825831da8a
alanluk2226 <83753838+alanluk2226@users.noreply.github.com>
2025-11-07 20:59:32 +0800

    Update User.js

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Create User model
+module.exports = mongoose.model('User', userSchema);
